759d433c2598954ca531805a6d4e6cd24e03afa1
Ivan Presinal <ivan.presinal@hotmail.com>
2021-08-04 11:37:19 -0400

    configuracion de proyecto application

---
+++ RESTfulBasics/Application/Features/Client/Commands/CreateClient/CreateClientCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Client.Commands.CreateClient
+{
+    public class CreateClientCommand
+    {
+    }
+}
---
+++ RESTfulBasics/Domain/Entities/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Client : AuditableBaseEntity
+    {
+        private int _age;
+
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public int Age {
+            get
+            {
+                if (this._age <= 0)
+                {
+                    _age = new DateTime(DateTime.Now.Subtract(DateOfBirth).Ticks).Year - 1;
+                }
+
+                return _age;
+            }
+        }
+    }
+}
